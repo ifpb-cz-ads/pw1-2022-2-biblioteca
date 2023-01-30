32f06d6182b0a48b462976fbb43486e1dae5eb3c
Davi Morais <davimorais847@gmail.com>
2023-01-30 20:24:15 -0300

    consertando as rotas

--- src/app.js
+++ src/app.js
@@ -5,4 +5,12 @@ const app = express();
 
 
 
+//as rotas do diretorio Routers:
+const rotas = require('./Routers/rotas');
+const api = require('./Routers/api');
+
+app.use(rotas);
+app.use('/api', api);
+
+
 app.listen(process.env.PORT || 3000, ()=> console.log("Aplicacao rodando"));
