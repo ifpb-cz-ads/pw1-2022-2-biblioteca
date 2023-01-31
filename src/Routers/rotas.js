f2388e845d8fab6b357c6520af4c5f513ea99134
Robson William <robsonwilliamro@gmail.com>
2023-01-31 14:23:26 -0300

    Adicionando página individual para o livro

--- src/Routers/rotas.js
+++ src/Routers/rotas.js
@@ -6,7 +6,9 @@ app.get('/', (req, res) => {
     res.render('index');
 })
 
-
+app.get('/book', (req, res) => {
+	res.render('book-page');
+})
 //aqui são as rotas que a gente decide quantas vai criar:
 
 module.exports = app;
