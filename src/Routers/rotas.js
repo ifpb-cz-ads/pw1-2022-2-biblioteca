a6442ab68a7540b1e45fe5b0aa37950902ca2e75
Robson William <robsonwilliamro@gmail.com>
2023-01-31 15:19:46 -0300

    Fazendo com que os items sejam gerados dinamicamente

--- src/Routers/rotas.js
+++ src/Routers/rotas.js
@@ -3,19 +3,7 @@ const app = require('express').Router();
 //aqui vai ser o detalhamento das entidades da aplicacao:
 //unica rota que nao precisa login:
 app.get('/', (req, res) => {
-    res.render('index');
-})
-
-app.get('/book', (req, res) => {
-	res.render('book-page');
-})
-
-app.get('/login', (req, res) => {
-	res.render('login');
-})
-
-app.get('/signin', (req, res) => {
-	res.render('signIn');
+    res.send('index');
 })
 //aqui são as rotas que a gente decide quantas vai criar:
 
