95a9db4993b8b0800aee6333645d1bede13f784d
Diuari <diuaripessoa07@gmail.com>
2023-02-06 21:59:10 -0300

    atualizando codigo quebrado

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -37,79 +37,41 @@ async function cadastrarUsuario(req,res){
     res.redirect('/')
 }
 
-// async function logarUsuario(req, res){
-//     const {email, senha} = req.body
-
-    //Checkando se o usuário está cadastrado
-    const user = await Usuario.findOne({email:email});
-
-    if(!user){
-        return res.status(404).json({msg:"Usuário não encontrado"});
-    }
-
-    //Checkando se a senha está correta
-    const checkPassword = await bcrypt.compare(senha, user.senha)
-    if(!checkPassword){
-        return res.status(422).json({msg: 'Senha inválida'})
-    }
-    
-//     if(req.body.senha == senha && req.body.email == email){
-//         //logado
-//         req.session.email = email;
-//         req.session.senha = senha;
-//     }
-
-//     try {
-//         const secret = `${process.env.SECRET}`
-//         const token = jwt.sign(
-//             {
-//             id: user._id,
-//             },
-//             secret,
-//         )
-//         //
-//         req.session.token = token;
-//         res.redirect('/')
-//     } catch (error) {
-//         console.log(error);
-//     }
-
-// }
-
 async function logarUsuario(req, res){
-    try {
-        const { email, senha } = req.body;
-    
-        const user = await Usuario.findOne({email:email});
-    
-        const match = await bcrypt.compare(senha, user.senha);
-    
-        if (match) {
-          const token = await jwt.sign(
-            { userId: user.id },
-            process.env.SECRET_KEY,
-            { expiresIn: 3600 } // 1h
-          );
-    
-          const tokenBearer = `Bearer ${token}`;
-    
-          req.session.user = user;
-    
-          res.cookie('access_token', tokenBearer, { maxAge: 3600000 }); // 1h
-          res.set('Authorization', tokenBearer);
-          res.redirect('/');
-        } else {
-          console.log('Senha inválida.');
-          req.flash('error', 'Senha inválida. Tente novamente.');
-          res.redirect('/signup');
-        }
-      } catch (error) {
-        console.log(error);
-        req.flash('error', 'Usuário não cadastrado. Realize seu cadastro.');
+  try {
+      const { email, senha } = req.body;
+  
+      const user = await Usuario.findOne({email:email});
+  
+      const match = await bcrypt.compare(senha, user.senha);
+  
+      if (match) {
+        const token = await jwt.sign(
+          { userId: user.id },
+          process.env.SECRET_KEY,
+          { expiresIn: 3600 } // 1h
+        );
+  
+        const tokenBearer = `Bearer ${token}`;
+  
+        req.session.user = user;
+  
+        res.cookie('access_token', tokenBearer, { maxAge: 3600000 }); // 1h
+        res.set('Authorization', tokenBearer);
+        res.redirect('/');
+      } else {
+        console.log('Senha inválida.');
+        req.flash('error', 'Senha inválida. Tente novamente.');
         res.redirect('/signup');
       }
+    } catch (error) {
+      console.log(error);
+      req.flash('error', 'Usuário não cadastrado. Realize seu cadastro.');
+      res.redirect('/signup');
+    }
 
 }
+
 
 
 
--- src/middlewares/middleware.js
+++ src/middlewares/middleware.js
@@ -17,3 +17,5 @@ exports.loginReq = (req, res, next) => {
   };
 
 
+
+
